9a4cba2238e43c5a4a7a9df73f2201c2deab29cd
Morten Knudsen <121301838+KnudsenMorten@users.noreply.github.com>
2024-08-18 13:54:21 +0200

    dev

---
+++ Az-PostMitigationsKnownIssues.ps1
@@ -0,0 +1,19 @@
+﻿#-----------------------------------------------------------------------------------------------------------------
+# Variables
+#-----------------------------------------------------------------------------------------------------------------
+    $global:TerminateSession = $false
+
+#-----------------------------------------------------------------------------------------------------------------
+# Mitigations
+#-----------------------------------------------------------------------------------------------------------------
+
+    If ([version]$Global:InstalledVersionMainModule.Version -ge [version]12.0.0 )
+        {
+            $Setting = get-azconfig -EnableLoginByWam
+            If ($Setting.Value -eq $true)
+                {
+                    # Fix AzV12 "Unable to acquire token for tenant 'organizations' with error 'InteractiveBrowserCredential authentication failed: A window handle must be configured"
+                    Update-AzConfig -EnableLoginByWam $false
+                    $global:TerminateSession = $true
+                }
+        }
