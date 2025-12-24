6f747b4c06c5cbae722c0e4a7c84f19c31610707
Morten Knudsen <121301838+KnudsenMorten@users.noreply.github.com>
2025-12-24 10:09:29 +0100

    cfg

--- Microsoft.Graph-PostMitigationsKnownIssues.ps1
+++ Microsoft.Graph-PostMitigationsKnownIssues.ps1
@@ -10,3 +10,20 @@
     # Forcing a maximum/specific version
     Write-host "Version: $($Global:InstalledVersionMainModule.Version)"
     write-host ""
+
+    # Forcing a maximum/specific version
+    $global:ModuleRequiredVersion = "2.33.0"
+
+    Write-host "Version: $($Global:InstalledVersionMainModule.Version)"
+    write-host ""
+
+    If ([version]$Global:InstalledVersionMainModule.Version -gt [version]$global:ModuleRequiredVersion )
+        {
+            write-host "Downgrade manually to v2.33.0 ... Please Wait !!"
+            write-host ""
+            
+            uninstall-module Microsoft.Graph
+            install-module Microsoft.Graph -RequiredVersion 2.33.0 -Force
+
+            $global:TerminateSession = $true
+        }
