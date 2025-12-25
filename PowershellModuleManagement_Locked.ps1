6412e1299895784926bdeba9d48432816e6cc55a
Morten Knudsen <121301838+KnudsenMorten@users.noreply.github.com>
2025-12-25 12:13:05 +0100

    Update PowershellModuleManagement_Locked.ps1

--- PowershellModuleManagement_Locked.ps1
+++ PowershellModuleManagement_Locked.ps1
@@ -228,51 +228,7 @@ ForEach ($Module in $Modules)
         #-----------------------------------------------------------------------------------------------------------------------------------------------------------
         # Check Installed versions
         #-----------------------------------------------------------------------------------------------------------------------------------------------------------
-            InstalledModuleInfoPsModuleManagement -MainModule $MainModule -AuthModule $AuthModule -MaintenancePowershellServices $MaintenancePowershellServices -MaintenancePowershellProcesses $MaintenancePowershellProcesses -CheckInstallation -ModuleRequiredVersion $ModuleRequiredVersion
-
-        #-----------------------------------------------------------------------------------------------------------------------------------------------------------
-        # Remove never auth. modules if found !
-        #-----------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    # Forcing a maximum/specific version
-    $global:ModuleRequiredVersion = "2.32.0"
-
-    If ([version]$Global:InstalledVersionAuthModule.Version -gt [version]$global:AuthMoeRequiredVersion )
-        {
-            write-host "Downgrade manually to $($global:ModuleRequiredVersion) ... Please Wait !!"
-            write-host ""
-            
-            # Force Microsoft Graph Authentication removal
-            Disconnect-MgGraph -ErrorAction SilentlyContinue
-            Remove-Module Microsoft.Graph.Authentication -Force -ErrorAction SilentlyContinue
-            Uninstall-Module Microsoft.Graph.Authentication -AllVersions -Force
-
-            # Force remove file in use
-            $graphPaths = Get-Module Microsoft.Graph.Authentication -ListAvailable |
-                          Select-Object -ExpandProperty Path
-
-            $versionFolders = $graphPaths | ForEach-Object { Split-Path $_ -Parent } | Sort-Object -Unique
-
-            $versionFolders | ForEach-Object {
-
-                $folder = $_
-                $folderVersion = Split-Path $folder -Leaf
-
-                if ($folderVersion -eq $global:ModuleRequiredVersion) {
-                    Write-Host "Skipping required version folder: $folder"
-                    return
-                }
-
-                Write-Host "Removing: $folder"
-                Remove-Item $folder -Recurse -Force -ErrorAction SilentlyContinue
-            }
-
-            uninstall-module Microsoft.Graph.Beta -AllVersions -Force
-            install-module Microsoft.Graph.Beta -RequiredVersion $global:ModuleRequiredVersion -Force
-
-            $global:TerminateSession = $true
-        }
-
+            InstalledModuleInfoPsModuleManagement -MainModule $MainModule -AuthModule $AuthModule -MaintenancePowershellServices $MaintenancePowershellServices -MaintenancePowershellProcesses $MaintenancePowershellProcesses -CheckInstallation -ModuleRequiredVersion $ModuleRequiredVersion -AuthModuleRequiredVersion $AuthModuleRequiredVersion
 
         #-----------------------------------------------------------------------------------------------------------------------------------------------------------
         # PostActionsPsModuleManagement - Important after initial implementation
