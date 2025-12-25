5715e9e87adf5d6da8c5a36638795bdc27baf857
Morten Knudsen <121301838+KnudsenMorten@users.noreply.github.com>
2025-12-25 09:44:13 +0100

    cfg

--- Microsoft.Graph-PostMitigationsKnownIssues.ps1
+++ Microsoft.Graph-PostMitigationsKnownIssues.ps1
@@ -12,18 +12,23 @@
     write-host ""
 
     # Forcing a maximum/specific version
-    $global:ModuleRequiredVersion = "2.33.0"
+    $global:ModuleRequiredVersion = "2.32.0"
 
     Write-host "Version: $($Global:InstalledVersionMainModule.Version)"
     write-host ""
 
     If ([version]$Global:InstalledVersionMainModule.Version -gt [version]$global:ModuleRequiredVersion )
         {
-            write-host "Downgrade manually to v2.33.0 ... Please Wait !!"
+            write-host "Downgrade manually to $($global:ModuleRequiredVersion) ... Please Wait !!"
             write-host ""
             
-            uninstall-module Microsoft.Graph
-            install-module Microsoft.Graph -RequiredVersion 2.33.0 -Force
+            # Force Microsoft Graph Authentication removal
+            Disconnect-MgGraph -ErrorAction SilentlyContinue
+            Remove-Module Microsoft.Graph.Authentication -Force -ErrorAction SilentlyContinue
+            Uninstall-Module Microsoft.Graph.Authentication -AllVersions -Force
+
+            uninstall-module Microsoft.Graph -AllVersions -Force
+            install-module Microsoft.Graph -RequiredVersion $global:ModuleRequiredVersion -Force
 
             $global:TerminateSession = $true
         }
